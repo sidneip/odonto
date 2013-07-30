ba6fdd6a2a8929cf4b76bc398e32e320bb522946
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-07-29 21:37:11 -0300

    mask inputs

--- app/models/procedimento.rb
+++ app/models/procedimento.rb
@@ -1,3 +1,7 @@
+#encoding: utf-8
 class Procedimento < ActiveRecord::Base
-  validates_presence_of :nome, :mensage => "nome nulo"
+  
+  #validações dos campos
+  validates_presence_of :nome, :message => "não pode ser em branco"
+  validates_numericality_of :custo, :allow_nil => true, :message => "deve ser numeros"
 end
