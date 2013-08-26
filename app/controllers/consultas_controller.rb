34e496176fb5290c8efa52fdda2404210f2455f4
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-08-26 00:05:54 -0300

    agenda dentista e views

--- app/controllers/consultas_controller.rb
+++ app/controllers/consultas_controller.rb
@@ -5,9 +5,14 @@ class ConsultasController < ApplicationController
   # GET /consultas
   # GET /consultas.json
   def index
-    @consultas = Consulta.page(params[:page]).per(8)
+    unless params[:search].try(:nil) || params[:search].try(:blank?)
+        @consultas = Consulta.joins(:paciente).where("pacientes.nome like ?", "%#{params[:search]}%").order("created_at DESC").page(params[:page]).per(8)
+    else
+        @consultas = Consulta.order("created_at DESC").page(params[:page]).per(8)
+    end
+    @consultasxml = Consulta.all
     respond_to do |format|
-        format.xml {render :xml => @consultas.to_xml(:include => :paciente) }
+        format.xml {render :xml => @consultasxml.to_xml(:include => :paciente) }
         format.json {render :json => @consultas}
         format.html {render :html => @consultas}
     end
@@ -75,6 +80,24 @@ class ConsultasController < ApplicationController
       format.html {render :html => @consultas}
     end
   end
+
+  def calendario_dentista
+    @consultas = Consulta.where(:dentista_id => params[:id])
+    respond_to do |format|
+      format.xml {render :xml => @consultas}
+      format.json {render :json => @consultas}
+      format.html {render :html => @consultas}
+    end
+  end
+
+  def confirmar_consulta
+    @consulta = Consulta.find(params[:id])
+    @consulta.update_attribute(:status, "Confirmado")
+    respond_to do |format|
+      format.html { redirect_to consultas_path, notice: "Consulta do paciente #{@consulta.paciente.nome} na data #{@consulta.data} confirmada" } 
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
 
@@ -86,4 +109,6 @@ class ConsultasController < ApplicationController
     def consulta_params
       params.require(:consulta).permit(:data, :hora_inicio, :hora_fim, :tipo, :procedimento_id, :paciente_id, :convenio_id, :dentista_id, :observacao, :status)
     end
+
+
 end
