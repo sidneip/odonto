e7a8fd6174f120f49a3d47168ba80d1081004005
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-08-27 01:47:46 -0300

    alteracoes na pagina consulta

--- app/models/descritivo_fatura.rb
+++ app/models/descritivo_fatura.rb
@@ -1,2 +1,3 @@
 class DescritivoFatura < ActiveRecord::Base
+  belongs_to :fatura
 end
--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -1,3 +1,4 @@
 class Fatura < ActiveRecord::Base
   belongs_to :consulta
+  has_many   :descritivo_fatura
 end
