faab1da03c39f0bd2232bfba31a0f70930162346
Sidnei Pacheco <sidnei@HDU15130.local>
2013-10-07 15:56:11 -0300

    add contexto de clinicas

--- app/models/clinica.rb
+++ app/models/clinica.rb
@@ -1,2 +1,15 @@
 class Clinica < ActiveRecord::Base
+
+  def self.authenticate(email, password)
+  	@user = find_by_email(email)
+  	if @user
+  		puts @user.senha
+  		puts password
+  	  if @user.senha == password
+  	  	return @user
+  	  else 
+  	  	nil
+  	  end
+    end
+  end
 end
