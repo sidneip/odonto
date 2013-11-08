b3d26a9fbad57e529ee4fd708ac913127dbcc39c
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-11-07 22:39:06 -0200

    autocomplete paciente

--- app/assets/javascripts/getEndereco.js
+++ app/assets/javascripts/getEndereco.js
@@ -1,13 +1,13 @@
 function getEndereco() {
-    if ($.trim($("#paciente_user_cep").val()) != "") {
-      $.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#paciente_user_cep").val(), function(){
+    if ($.trim($("#paciente_cep").val()) != "") {
+      $.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#paciente_cep").val(), function(){
       <!-- Atribuindo os valores para os componentes da página-->
     if(resultadoCEP["resultado"]){
-        $("#paciente_user_rua").val(unescape(resultadoCEP["tipo_logradouro"])+": "+unescape(resultadoCEP["logradouro"]));
+        $("#paciente_rua").val(unescape(resultadoCEP["tipo_logradouro"])+": "+unescape(resultadoCEP["logradouro"]));
         //$("#paciente_user_rua").val(unescape(resultadoCEP["logradouro"]));
-        $("#paciente_user_bairro").val(unescape(resultadoCEP["bairro"]));
-        $("#paciente_user_cidade").val(unescape(resultadoCEP["cidade"]));
-        $("#paciente_user_uf").val(unescape(resultadoCEP["uf"]));
+        $("#paciente_bairro").val(unescape(resultadoCEP["bairro"]));
+        $("#paciente_cidade").val(unescape(resultadoCEP["cidade"]));
+        $("#paciente_uf").val(unescape(resultadoCEP["uf"]));
         } else {
           alert("Cep não encontrado.");
        }
